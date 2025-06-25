7f11c9a903390cb8e441410ed54411cc76bd6c22
Pakelin <mcardenas.4094@unimar.edu.ve>
2025-06-25 03:32:08 -0400

    Creacion de ruta para src (de '../src' a '@') + cambios minimos al index

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react()],
+    react()
+  ],
   server: {
     host: 'localhost',
     port: 5173,
     https: {
-      key: '../backend/key.pem',
-      cert: '../backend/cert.pem',
+      key: path.resolve(__dirname, '../backend/key.pem'),
+      cert: path.resolve(__dirname, '../backend/cert.pem'),
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
